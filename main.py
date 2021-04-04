a5330a0e6b9d4af2b0a6ffbd9eb03a50def34a71
Atj Rupesh <62754636+atjrupesh@users.noreply.github.com>
2021-04-04 19:16:39 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
-# 🚨 Don't change the code below 👇
 height = float(input("enter your height in m: "))
 weight = int(input("enter your weight in kg: "))
-# 🚨 Don't change the code above 👆
 
-#Write your code below this line 👇
 
 BMI = float(weight / height**2) #You can add int instead of float if you want to discrad the float values.
 
