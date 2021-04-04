d7c089cdd0bcc4489dc0c60b5621f0a15ffcec5e
Atj Rupesh <62754636+atjrupesh@users.noreply.github.com>
2021-04-04 19:16:02 +0530

    Add files via upload

---
+++ main.py
@@ -0,0 +1,22 @@
+# 🚨 Don't change the code below 👇
+height = float(input("enter your height in m: "))
+weight = int(input("enter your weight in kg: "))
+# 🚨 Don't change the code above 👆
+
+#Write your code below this line 👇
+
+BMI = float(weight / height**2) #You can add int instead of float if you want to discrad the float values.
+
+n_BMI = str (BMI)
+
+print("Your BMI is " + n_BMI + ".")
+
+
+
+
+
+
+
+
+
+
